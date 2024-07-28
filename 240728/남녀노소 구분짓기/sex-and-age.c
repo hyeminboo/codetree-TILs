6f0cf2ba169a1faa9bedabf77656b9c6ee93999a
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-07-28 14:23:42 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/남녀노소 구분짓기/sex-and-age.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (a == 0) {
+        if (b >= 19) {
+            printf("MAN");
+        } else {
+            printf("BOY");
+        }
+    }
+    if (a == 1) {
+        if (b >= 19) {
+            printf("WOMAN");
+        } else {
+            printf("GIRL");
+        }
+    }
+    return 0;
+
+}
