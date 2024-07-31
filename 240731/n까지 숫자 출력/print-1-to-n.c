819d1c3634a101ccffe9aea2a58a46bfd8063c4a
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-07-31 19:30:05 +0900

    Title: n까지 숫자 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/n까지 숫자 출력/print-1-to-n.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i =1, n;
+    scanf("%d", &n);
+    
+    while (i <= n) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
