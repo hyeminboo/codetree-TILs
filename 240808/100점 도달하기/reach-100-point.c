6c7100a227ec864e93f89f84eb74820f0d77ad94
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-08 16:08:42 +0900

    Title: 100점 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/100점 도달하기/reach-100-point.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    for (int i = n; i <= 100; i++) {
+        if (i < 60) printf("F ");
+        else if (i < 70) printf("D ");
+        else if (i < 80) printf("C ");
+        else if (i < 90) printf("B ");
+        else printf("A ");
+    }
+    return 0;
+}
