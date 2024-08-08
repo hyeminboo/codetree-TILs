dcef8ee58a33a03236f5d75d6f8c71ab7a973321
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-08 16:24:33 +0900

    Title: a 부터 b 까지, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240808/a 부터 b 까지/a-to-b.c
+++ 240808/a 부터 b 까지/a-to-b.c
@@ -4,15 +4,13 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%d ", a);
 
-    for (int i = a; i < b;) {
-        if (i % 2 == 0) {
-            i += 3;
-            printf("%d ", i);
+    while (a < b) {
+        printf("%d ", a);
+        if (a % 2 == 0) {
+            a += 3;
         } else {
-            i *= 2;
-            printf("%d ", i);
+            a *= 2;
         }
     }
     return 0;
