311d4ba6cea972d188164402a90347361ab45068
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-08 16:28:12 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/369 게임/369-game.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        if ((i % 3 == 0) | (i % 6 == 0) | (i % 9 == 0)) {
+            printf("0 ");
+        } else {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
