af8dabc8697543179f461d2189addff54d73715d
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-10 11:27:47 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240810/청소당번 정하기/cleaning-numbering.c
+++ 240810/청소당번 정하기/cleaning-numbering.c
@@ -6,20 +6,14 @@ int main() {
     scanf("%d", &day);
 
     for (int i = 1; i <= day; i++) {
-        if (i % 36 == 0) 
+        if (i % 12 == 0)
             c += 1;
-        else if (i % 24 == 0)
-            c += 1;
-        else if (i % 6 == 0) 
+        else if (i % 3 == 0)
             b += 1;
         else if (i % 2 == 0)
             a += 1;
-        else if (i % 3 == 0)
-            b += 1;
-        else if (i % 12 == 0)
-            c += 1;
     }
-    
+
     printf("%d %d %d", a, b, c);
 
 
