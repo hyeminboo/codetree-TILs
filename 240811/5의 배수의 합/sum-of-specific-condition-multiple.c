cda7ef19d31a3058eabbf8b527955b93e7635528
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-11 13:32:12 +0900

    Title: 5의 배수의 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240811/5의 배수의 합/sum-of-specific-condition-multiple.c
+++ 240811/5의 배수의 합/sum-of-specific-condition-multiple.c
@@ -3,8 +3,14 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, sum = 0;
+    int start, end;
 
-    for (int i = a; i <= b; i++) {
+    if (a <= b) {
+        start = a; end = b;
+    } else
+        start = b; end = a;
+
+    for (int i = start; i <= end; i++) {
         if (i % 5 == 0) {
             sum += i;
         }
