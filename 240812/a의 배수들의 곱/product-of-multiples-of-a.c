e9cfd5c9a2464b6abb59b7770059c81a8d656098
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-12 16:24:39 +0900

    Title: a의 배수들의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240812/a의 배수들의 곱/product-of-multiples-of-a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int mul = 1
+
+    for (int i = 1; i <= b; i++) {
+        if (i % a == 0) {
+            mul *= i
+        }
+    }
+
+    printf("%d", mul);
+    return 0;
+}
