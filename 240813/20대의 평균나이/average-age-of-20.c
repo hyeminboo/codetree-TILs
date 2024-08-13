d27d2fb0a2b2c67327f8c8e4d418a2c52e6ff746
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-13 17:41:50 +0900

    Title: 20대의 평균나이, Time: 1082ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240813/20대의 평균나이/average-age-of-20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int age, sum = 0, cnt = 0;
+
+    while (1) {
+        scanf("%d", &age);
+        if (age < 30) {
+            sum += age;
+            cnt++;
+        } else {
+            printf("%.2lf", (double)sum/cnt);
+            break;
+        }
+    }
+    return 0;
+}
