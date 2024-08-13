814d4d91b92cc241bf5ee00a993e54c3d7fe8597
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-13 17:26:49 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n ;
+
+    while (1) {
+        scanf("%d", &n);
+        if (n < 25) {
+            printf("Higher\n");
+        } else if (n > 25) {
+            printf("Lower\n");
+        } else {
+            printf ("Good\n");
+            break;
+        }
+    } 
+    return 0;
+}
