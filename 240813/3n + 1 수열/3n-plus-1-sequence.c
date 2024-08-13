71cd8dc639d92879eda72a05f493268f25f84695
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-13 17:45:37 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/3n + 1 수열/3n-plus-1-sequence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt = 0;
+    scanf("%d", &n);
+
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+            cnt++;
+        } else {
+            n = n * 3 + 1;
+            cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+    return 0;
+}
