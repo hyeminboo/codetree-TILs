8d8e3be9e20f81bd06c3fdf4e9731a59f2197195
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-16 17:53:24 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/별표 출력하기 9/print-star-9.c
+++ 240816/별표 출력하기 9/print-star-9.c
@@ -12,9 +12,6 @@ int main() {
         for (int k = 0; k < 2*i+1; k++) {
             printf("* ");
         }
-        for (int j = 1; j < n-i; j ++) {
-            printf("  ");
-        }
         printf("\n");
     }   
     return 0;
