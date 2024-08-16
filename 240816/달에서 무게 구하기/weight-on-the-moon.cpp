0988a2654e0c7306d8a297bbfeca2949ca8a4459
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-16 22:43:36 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    cout.precision(6);
+
+    int a = 13;
+    double b = 0.165;
+
+    cout << a << " * " << b << " = " << a*b;
+    return 0;
+}
