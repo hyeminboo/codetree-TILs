042c42c91646736ce183f587bb874d1728fc5e28
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-19 14:14:30 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240819/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << "0" << a << "-" << c << "-" << b;
+
+    return 0;
+}
