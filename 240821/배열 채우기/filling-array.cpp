3abadbb6d24a047cee899efe5baa4a7ccc67858c
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-21 12:36:48 +0900

    Title: 배열 채우기, Time: 1062ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

---
+++ 240821/배열 채우기/filling-array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if (arr[i] == 0) {
+            for (int j = i-1; j >= 0; j--) {
+                cout << arr[j] << " ";
+            }
+            break;
+        }
+
+        if (i == 9) {
+        for (i; i >= 0; i--) {
+            cout << arr[i] << " ";
+        }
+    }
+    }
+    
+    return 0;
+}
